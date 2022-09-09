b7cd272584240cccc87f4b14c2d3e916bfdf9c30
audi <otiaudi9@gmail.com>
2022-09-09 18:08:31 +0300

    standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void);
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
+}
