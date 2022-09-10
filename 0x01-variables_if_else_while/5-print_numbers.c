24b45a37ea64ebd5e272416a59d0809cc8751857
audi <otiaudi9@gmail.com>
2022-09-10 03:39:04 +0300

    updated file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - This the entry point
+ *
+ * Return: always returns 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d\n", i);
+	}
+	return (0);
+}
