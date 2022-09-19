953c029b46cf2c400b3af2ad150821e068c01b05
audi <otiaudi9@gmail.com>
2022-09-19 09:55:06 +0300

    fixed the error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
- * Return: Always successfull
+ *
+ * Return: Always 0 (Succes)
  */
 void print_alphabet(void)
 {
-	int i;
+	int n;
 
-	for (i = 'a'; i <= 'z'; n++)
+	for (n = 'a'; n <= 'z'; n++)
 	{
-		_putchar(i);
+		_putchar(n);
 	}
 	_putchar('\n');
 }
