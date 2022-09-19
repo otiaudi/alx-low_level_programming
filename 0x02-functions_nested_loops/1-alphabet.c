55e5cafafa26b88ef8172d6d567a9989f90c0e88
audi <otiaudi9@gmail.com>
2022-09-19 09:27:45 +0300

    a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main  - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always successfull
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
