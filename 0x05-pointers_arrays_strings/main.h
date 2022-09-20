022453ba1e4cc07678829233f4cb6111ff438579
audi <otiaudi9@gmail.com>
2022-09-21 01:20:02 +0300

    hot fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,9 @@ void swap_int(int *a, int *b);
 void _puts(char *str);
 void rev_string(char *s);
 void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+void print_rev(char *s);
+int _strlen(char *s);
+
 #endif
