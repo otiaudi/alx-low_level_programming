be72747ac3f75ff4b49d2b10a186bdd97386293e
audi <otiaudi9@gmail.com>
2022-09-20 17:59:04 +0300

    A function to swap the value of two numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Function to swap the values of two integers
+ * @a: value to be swapped
+ * @b: value to be swapped
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	*a = temp;
+	*a = *b;
+	*b = temp;
+}
