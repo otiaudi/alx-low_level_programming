2b0ca8a5f8be1cea9d196f80a37655fdc3d04b2b
audi <otiaudi9@gmail.com>
2022-09-23 00:19:40 +0300

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * *_strncat - function that concatenates two strings
  * @dest:Destination sting.
@@ -10,6 +9,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strcat(dest, src, n);
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
