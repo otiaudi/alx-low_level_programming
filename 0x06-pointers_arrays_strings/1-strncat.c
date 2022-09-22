ae16a56cb461a952f4401f43ec0968020af258aa
audi <otiaudi9@gmail.com>
2022-09-23 00:02:34 +0300

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,6 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len = strlen(dest);
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; ++i)
-		dest[len + i] = src[i];
-		dest[len + i] = '\0';
-		return (dest);
+	strcat(dest, src, n);
+	return (dest);
 }
