82cbd84e2009d1690678adb6e10038b902df18aa
audi <otiaudi9@gmail.com>
2022-09-23 01:13:34 +0300

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0, op = 0;
+int i = 0, n = 0;
 
-	while (op == 0)
+	while (n == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		op = *(s1 + i) - *(s2 + i);
+		n = *(s1 + i) - *(s2 + i);
 		i++;
 	}
 
-	return (op);
+	return (n);
 }
 }
