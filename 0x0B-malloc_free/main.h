de7e19376a29162cbedb55c36f61955967b684eb
audi <otiaudi9@gmail.com>
2022-10-05 02:10:03 +0300

    function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+char *str_concat(char *s1, char *s2);
 int _putchar(char c);
+void free_grid(int **grid, int height);
 char *create_array(unsigned int size, char c);
 #endif
