827837a5da52f2fa31676237b3a5a970056c1d6b
audi <otiaudi9@gmail.com>
2022-10-05 02:14:28 +0300

    eturns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,5 @@ char *str_concat(char *s1, char *s2);
 int _putchar(char c);
 void free_grid(int **grid, int height);
 char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
 #endif
