ae1f3cdcf52f4447396022e50a80faaf608c569a
audi <otiaudi9@gmail.com>
2022-10-11 20:48:36 +0300

    defined the dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - stores characteristics of a dog
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif /*_DOG_H_*/
