ad057bc3c739bf2947de7eaeac22019cd17f9640
audi <otiaudi9@gmail.com>
2022-11-06 00:20:43 +0300

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,29 +10,32 @@
  * and returns -1 when filename is NULL.
  *
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int file1;		/*file descriptor*/
-	int count;		/* for looping throught the string*/
-	int nwr;		/* variable to store the result of write*/
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	file1 = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file1 == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
-		text_content = " ";
+		text_content = "";
 
-	for (count = 0; text_content[count]; count++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
-	nwr = write(file1, text_content, count);
-	if (nwr == -1)
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
 
-	close(file1);
+	close(fd);
+
 	return (1);
 }
