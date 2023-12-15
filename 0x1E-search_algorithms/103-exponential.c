bdbecaa5076688338826e34add5b5d3632f85964
otiaudi <otiaudi9@gmail.com>
2023-12-15 08:28:56 +0300

    updtad the file

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * exponential_search - searches for a value in an array of
+ * recursive_search - searches for a value in an array of
  * integers using the Binary search algorithm
  *
  *
@@ -10,7 +10,7 @@
  * @value: value to search in
  * Return: index of the number
  */
-int exponential_search(int *array, size_t size, int value)
+int recursive_search(int *array, size_t size, int value)
 {
 	size_t half = size / 2;
 	size_t i;
