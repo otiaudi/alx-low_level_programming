47d1d9f245548ed488ec0fd9f10246d5bb72457a
otiaudi <otiaudi9@gmail.com>
2023-12-15 08:28:01 +0300

    updated the file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,7 +12,7 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for alx project
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -30,6 +30,7 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
+ * for Holberton project
  */
 typedef struct skiplist_s
 {
